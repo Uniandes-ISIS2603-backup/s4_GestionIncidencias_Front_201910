4c697950537033f14814f8291fac90f5af29bb8d
v.parrac <v.parrac@uniandes.edu.co>
2019-04-10 17:21:31 -0500

    The begining of edit component

--- src/app/modules/user/tecnico/tecnico-edit/tecnico-edit.component.ts
+++ src/app/modules/user/tecnico/tecnico-edit/tecnico-edit.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { Component, OnInit, ViewContainerRef, Input, Output, EventEmitter } from '@angular/core';
 import { ActivatedRoute} from '@angular/router';
 import { TecnicoService } from '../tecnico.service';
 import { ModalDialogService, SimpleModalComponent } from 'ngx-modal-dialog';
 import { ToastrService} from 'ngx-toastr';
 import { Tecnico } from '../tecnico';
+
+
 
 
 @Component({
@@ -13,7 +15,41 @@ import { Tecnico } from '../tecnico';
 })
 export class TecnicoEditComponent implements OnInit {
 
-  constructor() { }
+  /**
+    * The component's constructor
+    * @param tecnicoService The editorial's service
+    * @param toastrService The toastr to show messages to the user 
+    */
+     constructor(
+      private tecnicoService: TecnicoService,
+      private toastrService: ToastrService
+    ) {}
+  
+    /**
+    * The id of the tecnico that the user wants to edit
+    * This is passed as a parameter by the parent component
+    */
+   @Input() tecnico_id: number;
+
+   /**
+   * The output which tells the parent component
+   * that the user no longer wants to create an tecnico
+   */
+   @Output() cancel = new EventEmitter();
+
+   /**
+   * The output which tells the parent component
+   * that the user updated a new tecnico
+   */
+   @Output() update = new EventEmitter();
+  
+   getTecnico: void {
+    this.editorialService.getEditorialDetail(this.editorial_id)
+        .subscribe(editorial => {
+            this.editorial = editorial;
+        });
+  }
+
 
   ngOnInit() {
   }
