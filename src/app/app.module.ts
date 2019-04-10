05b5457efe0d5bdfe0c289813b6901c21dcd2c45
v.parrac <v.parrac@uniandes.edu.co>
2019-04-09 21:24:27 -0500

    Upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ModalDialogModule } from 'ngx-modal-dialog';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing/app-routing.module';
 import {AuthModule} from './auth/auth.module';
+import { TecnicoComponent } from './components/tecnico/tecnico.component';
 
 
 
@@ -21,7 +22,8 @@ import {AuthModule} from './auth/auth.module';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        TecnicoComponent
     ],
     imports: [
         BrowserModule,
