b4a655143917569a2d70a2924432d44052735608
Juan Pablo Correa Puerta <jp.correap@uniandes.edu.co>
2019-04-09 20:45:23 -0500

    Creación de la clase usuario, cambio en el título de la aplicación

---
+++ src/app/usuario/usuario.ts
@@ -0,0 +1,16 @@
+/**
+ * Representa un usuario
+ * @author Juan Pablo Correa y Valerie Parra
+ */
+export class Usuario
+{
+    /**
+    * ID del usuario
+    */
+    id: number;
+
+    /**
+    * Nombre del usuario
+    */
+    name: string;
+}
