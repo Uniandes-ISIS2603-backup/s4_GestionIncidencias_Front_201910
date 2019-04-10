e40e0d50754dccc4a39d49debfc33412ff5b23dd
Juan Guillermo Lobaton Galindo <jg.lobaton10@uniandes.edu.co>
2019-04-10 14:09:12 -0500

    Agregado modulos service a actuacion

--- src/app/modules/actuacion/actuacion.service.ts
+++ src/app/modules/actuacion/actuacion.service.ts
@@ -23,9 +23,31 @@ getActuaciones(): Observable<Actuacion[]>{
     return this.http.get<Actuacion[]>(API_URL + actuaciones);
 }
 
+    /**
+    * Creates a new actuacion
+    * @paramactuacion The new actuacion
+    * @returns The actuacion with its new id if it was created, false if it wasn't
+    */
+   createBook(actuacion): Observable<Actuacion> {
+    return this.http.post<Actuacion>(API_URL + actuaciones, actuacion);
+}
+  /**
+        * Updates a new book
+        * @param book The updated book
+        * @returns The updated book
+        */
+       updateBook(actuacion): Observable<Actuacion> {
+        return this.http.put<Actuacion>(API_URL + actuaciones + '/' + actuacion.fecha, actuacion);
+    }
 
-
-
+ /**
+    * Borra una actuacion
+    * @param actuacionId el id de la actuacion a borrar
+    * @returns True if the book was deleted, false otherwise
+    */
+   deleteBook(actuacionId): Observable<Actuacion> {
+    return this.http.delete<Actuacion>(API_URL + actuaciones + '/' + actuacionId);
+}
 
 
 }
