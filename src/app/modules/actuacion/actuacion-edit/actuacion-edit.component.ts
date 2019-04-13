c85eaeece99b17b9a2f7f4ef8e50354ea203e677
jglobaton10 <47225250+jglobaton10@users.noreply.github.com>
2019-04-12 23:21:06 -0500

    Update actuacion-edit.component.ts

--- src/app/modules/actuacion/actuacion-edit/actuacion-edit.component.ts
+++ src/app/modules/actuacion/actuacion-edit/actuacion-edit.component.ts
@@ -7,7 +7,7 @@ import {ActuacionDetail} from '../actuacion-detail';
 
 @Component({
     selector: 'app-actuacion-edit',
-    templateUrl: './actuacion-edit.component.html',
+    templateUrl: './atuacion-edit.component.html',
     styleUrls: ['./actuacion-edit.component.css']
 })
 export class ActuacionEditComponent implements OnInit {
